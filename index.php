be2693eced12f984435bb4c7e7aa15f00debfc19
yassanzWebmaster <yassanz.webmaster@gmail.com>
2022-11-25 11:48:58 +0100

    update responsive

--- index.php
+++ index.php
@@ -66,43 +66,7 @@ require_once('library/function/required_function.php'); // Inclut les fonctions 
                         $tab = $res->fetchAll(); // Enregistrement du résultat sous forme de tableau
                         if ($tab) {
                             foreach ($tab as $key => $row) {
-
-                                // <div class=\"d-flex my-2\">
-                                //         <div class=\"flex-grow-1 w-75\">
-                                //             <h4 class=\"mt-0 mb-1\">" . $row["exp_libelle"] . ", " . $row["exp_societe"] . ", " . $row["exp_lieu"] . "</h4>
-                                //             <p class=\"lead\">
-                                //                 <span class=\"small\">" . $row["exp_debut_mois"] . ". " . $row["exp_debut_annee"] . " - " . $row["exp_fin_mois"] . ". " . $row["exp_fin_annee"] . " (" . $row["exp_duree"] . ")" . "</span>
-                                //             </p>
-                                //             <p>" . $row["exp_description"] . " <a data-bs-toggle=\"collapse\" href=\"#collapseExp" . $row["exp_id"] . "\" role=\"button\" aria-expanded=\"false\" aria-controls=\"collapseExp" . $row["exp_id"] . "\">Missions réalisées
-                                //         </a>
-                                //         <div class=\"collapse\" id=\"collapseExp" . $row["exp_id"] . "\">
-                                //             <div class=\"card card-body\">" . $row["exp_missions"] . "</div>
-                                //         </div>
-                                //         </p>
-                                //         </div>
-                                // </div>
-                                // ";
-                                echo "
-                                <div class=\"accordion-item\">
-                                <h2 class=\"accordion-header\" id=\"heading$key\">
-                                    <button class=\"accordion-button collapsed\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#collapse$key\" aria-expanded=\"true\" aria-controls=\"collapse$key\">
-                                    <div class=\"d-flex my-2\">
-                                    <div class=\"flex-grow-1 w-75\">
-                                        <h4 class=\"mt-0 mb-1\">" . $row["exp_libelle"] . ", " . $row["exp_societe"] . ", " . $row["exp_lieu"] . "</h4>
-                                        <p class=\"lead\">
-                                            <span class=\"small\">" . $row["exp_debut_mois"] . ". " . $row["exp_debut_annee"] . " - " . $row["exp_fin_mois"] . ". " . $row["exp_fin_annee"] . " (" . $row["exp_duree"] . ")" . "</span>
-                                        </p>
-                                        <p>" . $row["exp_description"] . "</p>
-                                    </button>
-                                </h2>";
-                                if ($row["exp_missions"]) {
-                                    echo "<div id=\"collapse$key\" class=\"accordion-collapse collapse\" \"aria-labelledby=\"heading$key\" data-bs-parent=\"#accordionExperiences\">
-                                    <div class=\"accordion-body\">
-                                        <strong>Missions réalisées</strong> " . $row["exp_missions"] . "
-                                    </div>
-                                </div>
-                            </div>";
-                                }
+                                // something
                             }
                         }
                         ?>
@@ -148,23 +112,25 @@ require_once('library/function/required_function.php'); // Inclut les fonctions 
         <!-- CONTACT -->
         <div id="contact">
             <div class="container-fluid">
-                <div class="p-5 row row-cols-lg-2 row-cols-md-1 row-cols-sm-1 bg-light d-flex">
+                <div class="p-5 row row-cols-1 row-cols-lg-2 row-cols-md-1 row-cols-sm-1 bg-light d-flex">
                     <!-- COORDONNÉES -->
                     <div class="col">
                         <h1 class="display-4">Me contacter</h1>
                         <p class="lead">Je serais ravi d'échanger avec vous.</p>
                         <hr class="my-4">
-                        <a data-toggle="tooltip" data-placement="right" title="yassanz.contact@gmail.com" href="mailto:yassanz.contact@gmail.com">
-                            <span class="bi bi-envelope m-2" style="font-size: 1.5rem; color: black;"></span>
-                        </a>
-                        <a data-toggle="tooltip" data-placement="right" title="07 52 70 17 42" href="tel:+33752701742">
-                            <span class="bi bi-telephone-fill m-2" style="font-size: 1.5rem; color: black;"></span>
-                        </a>
+                        <div class="row g-5">
+                            <a class="col-1" data-toggle="tooltip" data-placement="right" title="yassanz.contact@gmail.com" href="mailto:yassanz.contact@gmail.com">
+                                <span class="bi bi-envelope m-2" style="font-size: 1.5rem; color: black;"></span>
+                            </a>
+                            <a class="col-1" data-toggle="tooltip" data-placement="right" title="07 52 70 17 42" href="tel:+33752701742">
+                                <span class="bi bi-telephone-fill m-2" style="font-size: 1.5rem; color: black;"></span>
+                            </a>
+                        </div>
                     </div>
                     <!-- FORMULAIRE -->
                     <div class="col">
                         <form class="needs-validation" method="<?= $_SESSION["FORM_METHOD"] ?>" action="<?= $_SESSION["FORM_ACTION"] ?>" novalidate>
-                            <div class="row g-2 py-2">
+                            <div class="row row-cols-1 row-cols-lg-2 row-cols-md-1 row-cols-sm-1 g-2 py-2">
                                 <div class="col">
                                     <label class="form-label" for="_LastName">Nom : </label>
                                     <input type="text" value="<?php show_if_defined($_SESSION['_LastName']); ?>" class="text-uppercase form-control" name="_LastName" id="_LastName" required>
@@ -180,11 +146,11 @@ require_once('library/function/required_function.php'); // Inclut les fonctions 
                                     </div>
                                 </div>
                             </div>
-                            <div class="row g-1 py-2">
+                            <div class="col py-2">
                                 <label class="form-label" for="_Society">Société : <small class="text-muted">(facultatif)</small></label>
                                 <input type="text" class="form-control" name="_Society" id="_Society" novalidate />
                             </div>
-                            <div class="row g-1 py-2">
+                            <div class="col py-2">
                                 <label class="form-label" for="_Email">E-mail : </label>
                                 <input type="email" value="<?php show_if_defined($_SESSION['_Email']); ?>" class="text-lowercase form-control" name="_Email" id="_Email" required>
                                 <small id="emailHelp" class="form-text text-muted">Favoriser votre adresse mail professionnelle.</small>
@@ -192,22 +158,22 @@ require_once('library/function/required_function.php'); // Inclut les fonctions 
                                     Merci de renseigner une adresse mail valide.
                                 </div>
                             </div>
-                            <div class="row g-1 py-2">
+                            <div class="col py-2">
                                 <label class="form-label" for="_Object">Objet : </label>
                                 <input type="text" value="<?php show_if_defined($_SESSION['_Object']); ?>" class="form-control" name="_Object" id="_Object" required>
                                 <div class="invalid-feedback">
                                     Merci de renseigner l'objet de votre mail.
                                 </div>
                             </div>
-                            <div class="row g-1 py-2">
+                            <div class="col py-2">
                                 <label class="form-label" for="_Text">Votre message : </label>
                                 <textarea class="form-control" value="<?php show_if_defined($_SESSION['_Text']); ?>" name="_Text" id="_Text" rows="3" required></textarea>
                                 <div class="invalid-feedback">
                                     Votre mail doit contenir un message.
                                 </div>
                             </div>
-                            <div class="row g-1 py-2">
-                                <div class="g-recaptcha " data-sitekey="6LfbkVwiAAAAAPtNwsE5A2awWv2jvaNEhsSM7EmA"></div>
+                            <div class="col py-2">
+                                <div style="width:10px;" class="g-recaptcha" data-sitekey="6LfbkVwiAAAAAPtNwsE5A2awWv2jvaNEhsSM7EmA"></div>
                             </div>
                             <button type="submit" class="btn btn-primary">Envoyer</button>
                         </form>
@@ -326,15 +292,15 @@ require_once('library/function/required_function.php'); // Inclut les fonctions 
     </main>
     <!-- FOOTER -->
     <footer>
-            <nav class="nav flex-column p-5">
-                <h5>Sommaire</h5>
-                <a href="#accueil" class="nav-link text-muted">Accueil</a>
-                <a href="#qui-suis-je" class="nav-link text-muted">Qui-suis-je ?</a>
-                <a href="#experiences" class="nav-link text-muted">Expériences</a>
-                <a href="#formations" class="nav-link text-muted">Formations</a>
-                <a href="<?= $_SESSION['LINKEDIN_SRC'] ?>" class=" nav-link text-muted" target="_blank">LinkedIn</a></li>
-                <a href="#contact" class="nav-link text-muted">Contact</a></li>
-            </nav>
+        <nav class="nav flex-column p-5">
+            <h5>Sommaire</h5>
+            <a href="#accueil" class="nav-link text-muted">Accueil</a>
+            <a href="#qui-suis-je" class="nav-link text-muted">Qui-suis-je ?</a>
+            <a href="#experiences" class="nav-link text-muted">Expériences</a>
+            <a href="#formations" class="nav-link text-muted">Formations</a>
+            <a href="<?= $_SESSION['LINKEDIN_SRC'] ?>" class=" nav-link text-muted" target="_blank">LinkedIn</a></li>
+            <a href="#contact" class="nav-link text-muted">Contact</a></li>
+        </nav>
         </div>
         <div class="d-flex justify-content-between p-5 border-top">
             <p>&copy;<?= date('Y') . ' YassAnz Corporation. Tout droit réservé.' ?></p>
