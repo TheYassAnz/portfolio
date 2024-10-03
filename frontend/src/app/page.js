6ea30d9c56ac0bcc117f3d6043080ba7b6309085
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-10-03 12:54:07 +0200

    feat: create a maintenance page with next

---
+++ frontend/src/app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main class="flex items-center justify-center min-h-screen text-center p-6 rounded-lg shadow-lg">
+      <div>
+        <h1 class="text-4xl font-bold mb-4">Site en maintenance</h1>
+        <p class="text-lg mb-6">Nous effectuons actuellement une maintenance. </p>
+        <p><a href="https://www.linkedin.com/in/yanzarbasha/"><span class="bi bi-linkedin text-3xl" size="15"></span></a></p>
+      </div>
+    </main>
+  );
+}
