6996679680dc47343e70cbed71bce368c374cdd4
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2025-11-18 11:51:48 +0100

    feat: add Navbar and Button components; update layout and home page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from "@/components/navbar";
+
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Portfolio de Yassine ANZAR BASHA</h1>
     </div>
   );
