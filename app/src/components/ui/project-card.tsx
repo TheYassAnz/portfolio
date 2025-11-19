38e6f55a8dc0479054928c547e2399d0d2f767ed
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2025-11-19 12:03:45 +0100

    feat: enhance ProjectCard component with customizable background image

--- app/src/components/ui/project-card.tsx
+++ app/src/components/ui/project-card.tsx
@@ -1,12 +1,21 @@
 export default function ProjectCard({
   title = "Project Title",
   description = "A brief description of the project.",
+  backgroundImageUrl = "https://png.pngtree.com/thumb_back/fh260/background/20240522/pngtree-abstract-cloudy-background-beautiful-natural-streaks-of-sky-and-clouds-red-image_15684333.jpg",
 }: {
   title?: string;
   description?: string;
+  backgroundImageUrl?: string;
 }) {
   return (
-    <div className="flex flex-col min-w-[400px] h-[250px] border border-gray-300 rounded-md snap-start justify-between">
+    <div
+      className="flex flex-col min-w-[400px] h-[250px] border border-gray-300 rounded-md snap-start justify-between bg-cover bg-center"
+      style={
+        backgroundImageUrl
+          ? { backgroundImage: `url(${backgroundImageUrl})` }
+          : {}
+      }
+    >
       <div className="place-self-end p-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -23,7 +32,7 @@ export default function ProjectCard({
           />
         </svg>
       </div>
-      <div className="p-4">
+      <div className="p-4 backdrop-blur-sm bg-white/30 rounded-b-md">
         <h3 className="text-lg font-medium">{title}</h3>
         <p className="text-gray-500">{description}</p>
       </div>
