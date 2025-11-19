950ec053d7eb1af294540ad5332ff26bb2ffd246
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2025-11-19 09:31:32 +0100

    feat: add ProjectSection and ProjectCard components to showcase projects

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/hero-section";
 import Navbar from "@/components/navbar";
+import ProjectSection from "@/components/project-section";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <ProjectSection />
     </div>
   );
 }
---
+++ app/src/components/ui/project-card.tsx
@@ -0,0 +1,26 @@
+export default function ProjectCard() {
+  return (
+    <div className="flex flex-col min-w-[400px] h-[250px] border border-gray-300 rounded-md snap-start justify-between">
+      <div className="place-self-end p-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+          />
+        </svg>
+      </div>
+      <div className="p-4">
+        <h3 className="text-lg font-medium">Project Card Component</h3>
+        <p className="text-gray-500">Description of the project goes here.</p>
+      </div>
+    </div>
+  );
+}
